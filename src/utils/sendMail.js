c5494f156ff2abb6fe1a021c00cc3a3b09635e2e
Florien Niyongabo <waka@Floriens-MacBook-Pro-40797.local>
2024-10-15 09:43:44 +0200

    fix(puhs)

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -3,14 +3,9 @@ const nodemailer = require("nodemailer");
 async function sendEmail(mailOptions) {
   try {
     const transporter = nodemailer.createTransport({
-      // service: "gmail",
-      // auth: {
-      //   user: process.env.EMAIL_USER,
-      //   pass: process.env.EMAIL_PASSWORD,
-      // },
       host: "smtp.ethereal.email",
       port: 587,
-      secure: false, // true for port 465, false for other ports
+      secure: false, 
       auth: {
         user: "maddison53@ethereal.email",
         pass: "jn7jnAPss4f63QBp6D",
